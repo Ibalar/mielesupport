8ab2ffe462a110ec854542d8861afb84711f2eb0
Ibalar <ibalartutby@gmail.com>
2026-02-09 09:38:06 +0300

    обновление

--- wp-content/themes/miele-support/template-parts/blocks/faq.php
+++ wp-content/themes/miele-support/template-parts/blocks/faq.php
@@ -37,8 +37,24 @@ if (empty($faq_items)) {
                         <button class="faq__question" type="button" aria-expanded="false">
                             <span class="faq__question-text"><?php echo esc_html($item['question']); ?></span>
                             <span class="faq__icon">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 5V19M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <svg width="87" height="87" viewBox="0 0 87 87" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <g filter="url(#filter0_d_40000122_1588)">
+                                    <rect x="25.1992" y="25.2" width="36" height="36" rx="18" fill="#C5C5C5" />
+                                    <path d="M42.3757 36.2H44.0227V50.2H42.3757V36.2Z" fill="black" />
+                                    <path d="M50.1992 42.3765V44.0235L36.1992 44.0235L36.1992 42.3765L50.1992 42.3765Z" fill="black" />
+                                  </g>
+                                  <defs>
+                                    <filter id="filter0_d_40000122_1588" x="-0.000782967" y="-4.76837e-06" width="86.4" height="86.4" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                      <feMorphology radius="8.4" operator="erode" in="SourceAlpha" result="effect1_dropShadow_40000122_1588" />
+                                      <feOffset />
+                                      <feGaussianBlur stdDeviation="16.8" />
+                                      <feColorMatrix type="matrix" values="0 0 0 0 0.513333 0 0 0 0 0.5412 0 0 0 0 0.55 0 0 0 0.15 0" />
+                                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_40000122_1588" />
+                                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_40000122_1588" result="shape" />
+                                    </filter>
+                                  </defs>
                                 </svg>
                             </span>
                         </button>
